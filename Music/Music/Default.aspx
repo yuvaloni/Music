b5ff3530c4792abd8369f6c3f95ffd8a90ee46e9
yuvaloni <yuvon2@gmail.com>
2014-05-10 23:41:57 +0300

    first

---
+++ Music/Music/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Music.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        adress:
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        is comnnected:
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Start Server" />
+        <br />
+        song:
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Upload" />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Height="274px" Width="332px"></asp:ListBox>
+    
+    </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Play" Width="463px" />
+        </p>
+    </form>
+
+</body>
+</html>
---
+++ Music/Music/Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+namespace Music
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        int id = 0;
+        static Socket s;
+        static Socket c;
+        string play;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            s = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            TextBox1.Text = localIP +":1234";
+            TextBox2.Text = "no";
+        }
+        public bool isConnected = false;
+        public void Listen()
+        {
+            s.Bind(new IPEndPoint(IPAddress.Any, 1234));
+            s.Listen(1);
+            c = s.Accept();
+            TextBox2.Text = "yes";
+            isConnected = true;
+
+
+            
+        }
+        public void UploadFile()
+        {
+            string p = Path.Combine(Server.MapPath("~"), id + ".mp3");
+            id++;
+            File.WriteAllBytes(p, FileUpload1.FileBytes);
+            ListBox1.Items.Add(p);
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Play();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            UploadFile();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Listen();
+        }
+        public void Play()
+        {
+
+                     while(ListBox1.Items.Count>0)
+                     {
+                         c.SendFile(ListBox1.Items[0].ToString());
+                         ListBox1.Items.RemoveAt(0);
+                         c.Receive(new byte[1]);
+
+                     }
+        }
+
+    }
+}
